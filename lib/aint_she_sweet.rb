737a17ea90d36f3ef64a9c3628347bcee226a73b
Apprentice <apprentice@dbc06.local>
2014-07-19 13:06:24 -0700

    Implement Command line inteface for creating & renaming sinatra skeleton repo

---
+++ lib/aint_she_sweet.rb
@@ -0,0 +1,17 @@
+require "aint_she_sweet/version"
+
+module AintSheSweet
+  APPNAME = ARGV.pop
+  COMMAND = ARGV.join(" ")
+
+
+  def self.run
+    if COMMAND == "sweet sinatra"
+      setup
+    end
+  end
+
+  def self.setup
+    `git clone https://github.com/Sinatrify/Sinatra_Skeleton.git && rm -rf .git && mv Sinatra_Skeleton #{APPNAME}`
+  end
+end

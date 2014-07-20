6cd3f991e31f34fd5fe176456efdc07ada2a728e
Puru Dahal <puru@dahal.co>
2014-07-19 21:12:42 -0700

    Ready for Hackathon

--- lib/sweetsinatra.rb
+++ lib/sweetsinatra.rb
@@ -30,7 +30,7 @@ module SweetSinatra
     name = table.capitalize
 
     #model
-   `rake generate:model NAME=#{name}`
+    `rake generate:model NAME=#{name}`
     #migration
     filename = "%s_create_%s.rb" % [Time.now.strftime('%Y%m%d%H%M%S'), table]
     #path     = APP_ROOT.join('db', 'migrate', filename)
@@ -61,9 +61,73 @@ module SweetSinatra
     EOF
   end
   #calling controller creation
-  `rake generate:controller NAME=#{name}`
+  controller_name     = (name + 's_controller').capitalize
+  controller_filename =  name.downcase + '.rb'
+  controller_path = "app/controllers/#{controller_filename}"
 
-  #views
-  `rake generate:views NAME=#{name}`
+  if File.exist?(controller_path)
+    raise "ERROR: controller file '#{controller_path}' already exists"
   end
+
+  puts "Creating #{controller_path}"
+  File.open(controller_path, 'w+') do |f|
+    f.write(<<-EOF)
+    get '/' do
+      @#{name.downcase} = #{name}.all
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/new' do
+     #{name}.create(params)
+     redirect to('/')
+   end
+
+   get '/show/:id' do
+    @#{name.downcase} = #{name}.find(params[:id])
+  end
+  EOF
+end
+  #views
+  view_filename = name.downcase + '.erb'
+  view_path = "app/views"
+
+  if File.exist?(view_path)
+    raise "ERROR: view file '#{view_path}' already exists"
+  end
+
+  puts "Creating #{view_path}"
+  `mkdir app/views`
+  index = "index.erb"
+  create = "new.erb"
+  show = "show.erb"
+  `touch app/views/index.erb app/views/new.erb app/views/show.erb`
+  arr = [index, create, show]
+  arr.each do |path|
+    File.open(view_path + path, 'w+') do |f|
+      if path == index
+        f.write(<<-EOF)
+        <% @#{name.downcase}.each do |#{name.downcase}| %>
+          <%= #{name.downcase}.name %>
+          <% end %>
+          EOF
+      elsif path == create
+          f.write(<<-EOF)
+          <form action='' method='post'>
+          <input>
+          <input>
+          </form>
+          EOF
+      else
+          f.write(<<-EOF)
+          <h1>SHOW</h1>
+          EOF
+      end
+    end
+  end
+
+end
 end
